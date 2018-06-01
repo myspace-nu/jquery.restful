f8f36bbd70c049715e7963a8c1a1f340e6ef688b
myspace-nu <32058020+myspace-nu@users.noreply.github.com>
2018-06-01 14:51:44 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
 # jquery.restful
+
+A jQuery plugin for communicating with REST APIs.
+
+## Installation
+
+Include script *after* the jQuery library
+
+    <script src="/path/to/jquery.cookie-consent.min.js"></script>
+
+## Usage
+
+Sending data from a HTML form to a REST API and log the answer.
+
+	<form class="myClass" action="https://jsonplaceholder.typicode.com/posts" method="post">
+		<input type="text" name="foo" value="bar">
+		<input type="text" name="userId" value="1">
+		<input type="submit" value="Go">
+	</form>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('.myClass').restful({
+				onSuccess:function(data){
+					console.log(data);
+				}
+			});
+		});
+	</script>
+
+Sending data from a HTML link to a REST API, converting all data attributes to JSON.
+
+	<a class="myClass" data-foo="bar" href="https://jsonplaceholder.typicode.com/posts">Go</a>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('.myClass').restful({
+				onSuccess:function(data){
+					console.log(data);
+				}
+			});
+		});
+	</script>
+
+Using GET method and paramaters as part of the URL.
+
+	<a class="myClass" data-id="1" href="posts/{id}/comments">Go</a>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('.myClass').restful({
+				method:'GET',
+				url:'https://jsonplaceholder.typicode.com',
+				onSuccess:function(data){
+					console.log(data);
+				}
+			});
+		});
+	</script>
+
+## Options
+**dataType** - Data type to use
+
+    dataType: 'form'
+
+*Default: 'json'*
+
+**defaultValue** - Default value to use for URL paramaters
+
+    defaultValue: 'foo'
+
+*Default: null*
+
+**url** - Base URL for the API
+
+    url: 'https://jsonplaceholder.typicode.com'
+	
+**method** - HTTP method / Verb to use
+
+    method: 'GET'
+
+*Default: 'POST'*
+
+**onComplete** - Function called after completed request
+
+	onComplete:function(data){
+		console.log({ element:this, data:data });
+	}
+
+**onError** - Function called in case an error was raised
+
+	onError:function(data){
+		console.log({ element:this, data:data });
+	}
+
+**onSuccess** - Function called after a successful request
+
+	onSuccess:function(data){
+		console.log({ element:this, data:data });
+	}
+
+### Author: [Johan Johansson](https://github.com/myspace-nu)
---
+++ src/jquery.restful.js
@@ -0,0 +1,110 @@
+/*!
+ * jQuery restful plugin 0.1
+ * https://github.com/myspace-nu
+ *
+ * Copyright 2018 Johan Johansson
+ * Released under the MIT license
+ */
+(function($) {
+  $.fn.serializeObject = function() {
+		var obj = {};
+		var arr = this.serializeArray();
+		$.each(arr, function() {
+			if (obj[this.name]) {
+				if (!obj[this.name].push) {
+					obj[this.name] = [obj[this.name]];
+				}
+				obj[this.name].push(this.value || '');
+			} else {
+				obj[this.name] = this.value || '';
+			}
+		});
+		return obj;
+	};
+	$.fn.restful = function(options) {
+		var settings = $.extend({
+			url: null,
+			method: 'POST',
+			onComplete: function(){ },
+			onSuccess: function(){ },
+			onError: function(){ },
+			dataType: 'json',
+			defaultValue: null
+		}, options);
+		if(settings.url)
+			settings.url=settings.url.replace(/\/+$/,'');
+		this.each(function() {
+		  	if(this.tagName.toLowerCase() == 'form'){
+				$(this).submit(function(event) {
+					event.preventDefault();
+					var data = $(this).serializeObject();
+					var url = $(this).attr("action");
+					if(!url.match(/^(\w{3,5}\:)?\/+/) && settings.url){
+						url = settings.url + "/" + url.replace(/^\/+/,'');
+					}
+					var matches = url.match(/{+\:?\w+}+/g);
+					for (var i in matches){
+						var name = matches[i].match(/\w+/);
+						url = url.replace(matches[i],data[name[0]] || data[name[0].toLowerCase()] || settings.defaultValue);
+						delete data[name[0]];
+					}
+					var as = {
+						url: url,
+						type: $(this).attr('method') || settings.method,
+						context:this,
+						success: function(json){ settings.onSuccess.call(this,json); },
+						error: function(json){ settings.onError.call(this,json); },
+						complete: function(json){ settings.onComplete.call(this,json); }
+					};
+					if(as.type.toLowerCase() == 'get'){
+						as.url += '?'+$.param(data);
+					} else {
+						if(settings.dataType.toLowerCase() == 'json'){
+							as.contentType = 'application/json; charset=utf-8';
+							as.dataType = 'json';
+							as.data = JSON.stringify(data);
+						} else {
+							as.data = data;
+						}
+					}
+					$.ajax(as);
+				});
+			} else { // if(['a','button','input','img'].includes(this.tagName.toLowerCase()))
+				$(this).click(function(event) {
+					event.preventDefault();
+					var data = $(this).data();
+					var url = $(this).attr('href');
+					if(!url.match(/^(\w{3,5}\:)?\/+/) && settings.url){
+						url = settings.url + "/" + url.replace(/^\/+/,'');
+					}
+					var matches = url.match(/{+\:?\w+}+/g);
+					for (var i in matches){
+						var name = matches[i].match(/\w+/);
+						url = url.replace(matches[i],data[name[0]] || data[name[0].toLowerCase()] || settings.defaultValue);
+						delete data[name[0].toLowerCase()];
+					}
+					var as = {
+						url: url,
+						type: settings.method,
+						context:this,
+						success: function(json){ settings.onSuccess.call(this,json); },
+						error: function(json){ settings.onError.call(this,json); },
+						complete: function(json){ settings.onComplete.call(this,json); }
+					}
+					if(as.type.toLowerCase() == 'get'){
+						as.url += '?'+$.param(data);
+					} else {
+						if(settings.dataType.toLowerCase() == 'json'){
+							as.contentType = 'application/json; charset=utf-8';
+							as.dataType = 'json';
+							as.data = JSON.stringify(data);
+						} else {
+							as.data = data;
+						}
+					}
+					$.ajax(as);
+				});
+			}
+		});
+	};
+}(jQuery));
